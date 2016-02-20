170c855252c0244cb766fae820d6aada617a41eb
Daniel B <daniel.betz@daniel-betz.com>
2016-02-21 00:06:34 +0100

    Completed tileserver, pending refactorings

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	RegisterTileDatabase("/tmp/bla.db", "")
+	RegisterTileDatabase("/Users/fuzzy/Downloads/pirate-map.mbtiles")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", GetTile)
 	graceful.Run("127.0.0.1:8080", 1 * time.Second, mux)
--- tilehandler.go
+++ tilehandler.go
@@ -3,7 +3,6 @@ import (
 	"database/sql"
 	"net/http"
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 	_ "github.com/mattn/go-sqlite3"
@@ -14,59 +13,124 @@ import (
 var databases map[string]string = make(map[string]string)
 
 // Add tile database
-func RegisterTileDatabase(path string, name string) (err error) {
+func RegisterTileDatabase(path string, name ...string) (err error) {
 	if len(path) == 0 {
 		err = errors.New("Path must not be empty")
 		return
 	}
 
-	if len(name) == 0 {
-		_, file := filepath.Split(path)
-		name = file
+	_, file := filepath.Split(path)
+	databaseName := file
+	if len(name) > 0 {
+		databaseName = name[1]
 	}
 
-	databases[name] = path
+	databases[databaseName] = path
 	return
 }
 
 func GetTile(w http.ResponseWriter, r *http.Request) {
-	pathComponents := strings.Split(r.URL.Path[1:], "/")
+	pathComponents, err := prepareRequest(r.URL)
 
-	// expected: db, z, x, y
-	if len(pathComponents) != 4 {
-		http.NotFound(w, r)
+	// TODO: Cleanup of error processing
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	x, y, z, err := getCoordinates(pathComponents)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dsn, err, http_code := getDsn(pathComponents)
+	if err != nil {
+		http.Error(w, err.Error(), http_code)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		http.Error(w, "Database error (1)", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	var tile_data *sql.RawBytes
+
+	// TODO: Use .Query to properly handle "tile not found"
+	err = db.QueryRow("SELECT tile_data FROM tiles WHERE zoom_level = ? AND tile_row = ? AND tile_column = ?", z, y, x).Scan(&tile_data)
+	if err != nil {
+		http.Error(w, "Database error (2)", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "image/png")
+	w.Write(*tile_data)
+}
+
+func prepareRequest(requestUrl *url.URL) (pathComponents []string, err error) {
+	pathComponents = strings.Split(requestUrl.Path[1:], "/")
+
+	// expected: db, z, x, y
+	if len(pathComponents) != 4 {
+		err = errors.New("Unexpected number of path components")
+	}
+
+	return
+}
+
+func getDsn(pathComponents []string) (dsn string, err error, http_code int) {
 	dbPath, ok := databases[pathComponents[0]]
 	if !ok {
-		http.NotFound(w, r)
+		err = errors.New("Unknown database")
+		http_code = http.StatusNotFound
 		return
 	}
 
 	dbUri, err := url.Parse("file:" + dbPath)
 	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+		err = errors.New("Bad database")
+		http_code = http.StatusInternalServerError
+		return
 	}
 
-	dsn := dbUri.String() + "?cache=shared&immutable=1"
-	db, err := sql.Open("sqlite3", dsn)
+	dsn = dbUri.String() + "?cache=shared&immutable=1"
+	return
+}
 
+func getCoordinates(pathComponents []string) (x uint, y uint, z uint, err error) {
 	indexOfPeriod := strings.LastIndex(pathComponents[3], ".")
 	if indexOfPeriod != -1 {
 		pathComponents[3] = pathComponents[3][:indexOfPeriod]
 	}
 
-	z, errz := strconv.Atoi(pathComponents[1])
-	x, errx := strconv.Atoi(pathComponents[2])
-	y, erry := strconv.Atoi(pathComponents[3])
+	z64, err_z := strconv.ParseUint(pathComponents[1], 10, 0)
+	x64, err_x := strconv.ParseUint(pathComponents[2], 10, 0)
+	y64, err_y := strconv.ParseUint(pathComponents[3], 10, 0)
 
-	if errz != nil || errx != nil || erry != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+	if err_z != nil {
+		err = err_z
+	}
+
+	if err_x != nil {
+		err = err_x
+	}
+
+	if err_y != nil {
+		err = err_y
+	}
+
+	if err_z != nil || err_x != nil || err_y != nil {
 		return
 	}
 
-	fmt.Fprintf(
-		w, "Would fetch tile (z, x, y) %v, %v, %v in database %s\n", z, x, y, pathComponents[0])
-	fmt.Fprintf(w, "Database URI: %q", dbUri)
+	// mbtiles format stores y axis reversed
+	y64 = (1 << z64) - y64 - 1
+
+	x = uint(x64)
+	y = uint(y64)
+	z = uint(z64)
+	return
 }
